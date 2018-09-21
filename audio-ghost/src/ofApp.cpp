8e9364f433b91b5f4470b65c0b676eebc164ef4e
Valerio Viperino <valerio.viperino@gmail.com>
2018-09-21 18:05:58 +0200

    testing sound every 5 minutes

--- audio-ghost/src/ofApp.cpp
+++ audio-ghost/src/ofApp.cpp
@@ -5,6 +5,8 @@ void ofApp::setup(){
 
     // load sounds into the appropriate vectors
     load_sounds("rattle", "wav", &rattle_sounds);
+    can_play_sound = true;
+    start_time = steady_clock::now();
 }
 
 //--------------------------------------------------------------
@@ -12,6 +14,22 @@ void ofApp::update(){
     
     // update the sound playing system
     ofSoundUpdate();
+
+    // check the elapsed time
+    current_time = steady_clock::now();
+    int elapsed_seconds = duration_cast<seconds>(current_time - start_time).count();
+    int millis_modulo = ofGetElapsedTimeMillis() % 1000;
+    int seconds_interval = 5 * 60;
+    can_play_sound = elapsed_seconds % seconds_interval == 1;
+
+    // ofLogNotice() << "Elapsed seconds: " << elapsed_seconds;
+    // ofLogNotice() << "can_play_sound: " << can_play_sound;
+    // ofLogNotice() << "millis_modulo:  " << millis_modulo;
+
+    if (can_play_sound){
+        if (millis_modulo < 25)
+        play_random_sound(&rattle_sounds);
+    }
 }
 
 //--------------------------------------------------------------
@@ -22,14 +40,7 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if (key == 'p'){
-        int random_index = (int) floor(ofRandom(rattle_sounds.size()));
-        try {
-            ofLogNotice() << "playing back sound at index " << random_index;
-            rattle_sounds.at(random_index).play();
-        }
-        catch (bad_alloc &e){
-            ofLogError() << "error while playing back sound at index " << random_index;
-        }
+        play_random_sound(&rattle_sounds);
     }
 }
 
@@ -47,5 +58,18 @@ void ofApp::load_sounds(string path, string file_ext, vector<ofSoundPlayer> * so
         sound.load(dir.getPath(i));
         
         sounds->push_back(sound);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::play_random_sound(vector<ofSoundPlayer> * sounds){
+
+    int random_index = (int) floor(ofRandom(sounds->size()));
+    try {
+        ofLogNotice("play_random_sound") << "playing back sound at index " << random_index;
+        sounds->at(random_index).play();
+    }
+    catch (bad_alloc &e){
+        ofLogError("play_random_sound") << "error while playing back sound at index " << random_index;
     }
 }
--- audio-ghost/src/ofApp.h
+++ audio-ghost/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+using namespace std::chrono;
 
 class ofApp : public ofBaseApp{
 
@@ -12,7 +13,11 @@ class ofApp : public ofBaseApp{
 		void keyPressed(int key);
 
 		void load_sounds(string path, string file_ext, vector<ofSoundPlayer> * sounds);
+		void play_random_sound(vector<ofSoundPlayer> * sounds);
 
 		vector<ofSoundPlayer> rattle_sounds;
-	
+
+
+		steady_clock::time_point start_time, current_time;
+		bool can_play_sound;
 };
